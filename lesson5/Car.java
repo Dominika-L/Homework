028e0d33396578798ac7f24cbf0fd554d3c71c58
Dominika-L <73245442+Dominika-L@users.noreply.github.com>
2021-02-06 14:27:28 +0100

    Update Car.java

--- lesson5/Car.java
+++ lesson5/Car.java
@@ -1,11 +1,10 @@
 package homeworks.lesson5;
 
-public class Car extends Transport {
-
+public class Car extends Transport{
     public static void main(String[] args) {
 
         Transport car = new Transport();
 
-        car.ownProperty();
+        car.information();
     }
 }
