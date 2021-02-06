9b40047b571c2134efd8b97b43cd37ad906462a6
Dominika-L <73245442+Dominika-L@users.noreply.github.com>
2021-02-06 14:28:43 +0100

    Update Train.java

--- lesson5/Train.java
+++ lesson5/Train.java
@@ -1,12 +1,11 @@
 package homeworks.lesson5;
 
-public class Train extends PublicTransport {
-
+public class Train extends PublicTransport  {
     public static void main(String[] args) {
+
         PublicTransport train = new PublicTransport();
 
-        train.buyTicket();
-
+        train.information();
     }
-
 }
+
